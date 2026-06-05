1683fe8a752022a5971e14dbf3e6164ac3de725a
Kissapod <akselkotic@gmail.com>
2026-06-05 21:56:50 +0300

    Added New Entity User

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public void register(@RequestBody User user){
+        userService.register(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
